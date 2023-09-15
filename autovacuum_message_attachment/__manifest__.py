809f1ffd8ecc2d9b5ae665d666ae51a42d02fabc
Szeka Wong <szeka.wong@xcg-consulting.fr>
2023-09-15 12:45:02 +0200

    [ADD] autovacuum_message_attachment: Add variable to limit deleted objects per job

--- autovacuum_message_attachment/__manifest__.py
+++ autovacuum_message_attachment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "AutoVacuum Mail Message and Attachment",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "category": "Tools",
     "website": "https://github.com/OCA/server-tools",
     "author": "Akretion, Odoo Community Association (OCA)",
---
+++ autovacuum_message_attachment/readme/HISTORY.rst
@@ -0,0 +1,9 @@
+11.0.1.1.0
+~~~~~~~~~~
+
+* Add variable to limit the number of objects deleted per operation.
+
+11.0.1.0.0
+~~~~~~~~~~
+
+* Initial release.
